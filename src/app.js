e33d91983065a8e9d1bcf15c84dbd019c89da68c
obrian sanchez <obri10238@gmail.com>
2025-09-12 19:48:29 -0500

    dorian

--- src/app.js
+++ src/app.js
@@ -21,6 +21,10 @@ app.get("/version", (_req, res) => {
 app.get("/obrian", (_req, res) => {
   res.json({ mensaje: "es el mejor: ", persona: "obrian" });
 });
+
+app.get("/dorian", (_req, res) => {
+  res.json({ mensaje: "Quien moda es: ", persona: "Dorian" });
+});
  
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
